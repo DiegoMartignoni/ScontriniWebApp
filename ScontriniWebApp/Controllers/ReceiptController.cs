56affd2367a7948a5b9e9d2f6628516a57a5c6ba
Diego Martignoni <diego.martignoni@abenergie.it>
2020-02-06 12:34:08 +0100

    Eseguito restyling grafico generale, grafica scontrini e vari menu di interazione

--- ScontriniWebApp/Controllers/ReceiptController.cs
+++ ScontriniWebApp/Controllers/ReceiptController.cs
@@ -18,11 +18,14 @@ namespace ScontriniWebApp.Controllers
         }
         public IActionResult Index()
         {
+            ViewData["ComingFrom1"] = "Dashboard";
+            ViewData["Title"] = "Lista Scontrini";
             return View();
         }
 
         public async Task<IActionResult> List()
         {
+            ViewData["ComingFrom1"] = "Dashboard";
             ViewData["Title"] = "Lista Scontrini";
             List<ReceiptViewModel> receipts = await receiptService.GetReceiptsAsync();
             return View(receipts);
@@ -30,7 +33,8 @@ namespace ScontriniWebApp.Controllers
 
         public async Task<IActionResult> Detail(int id)
         {
-            ViewData["ComingFrom"] = "Lista Scontrini";
+            ViewData["ComingFrom1"] = "Dashboard";
+            ViewData["ComingFrom2"] = "Lista Scontrini";
             ViewData["Title"] = "Dettaglio Scontrino";
             ReceiptDetailViewModel receipt = await receiptService.GetReceiptAsync(id);
             return View(receipt);
